12dc800c5d5e574c8aa97217b8439683586aed6d
reem <hlhlstar@naver.com>
2022-12-10 17:23:06 +0900

    lec#4.1

--- bymyself/chap4/app.js
+++ bymyself/chap4/app.js
@@ -10,8 +10,11 @@ const loginBtn = loginForm.querySelector("button");
 // querySelector 안에선 CSS selector 방식으로 검색. #~~ -> id / .~~ -> class
 
 function onLoginBtnClick(){
-    console.log("hello ", loginInput.value);
-    console.log(`hi ${loginInput.value}`);
+    const userName = loginInput.value; //loginInput.value를 변수화
+    console.log("hello ", userName);
+    console.log(`hi ${userName}`);
 }
-
+/* userName에 아무 값도 작성되지 않은 상태로 loginBtn 눌렀을 땐 console.log 작동함.
+-> 이땐 required라는 유효성이 작성된 것이고, form이 submit된 게 아니기 때문에 브라우저 새로고침되지 않았기 때문.
+ */
 loginBtn.addEventListener("click", onLoginBtnClick);
