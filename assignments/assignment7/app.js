07a451880d4789b5e34ab006995178eb0008ac4b
reem <hlhlstar@naver.com>
2023-01-10 22:57:35 +0900

    Math.random, Math.ceil/round/floor 보충 설명

--- assignments/assignment7/app.js
+++ assignments/assignment7/app.js
@@ -14,7 +14,10 @@ function onNumSubmit(event){
     // span에는 guessedNum 값 출력되야함.
     const guessedNum = guessedNumInput.value;
     const maxNum = maxNumInput.value;
-    result.innerHTML = `You chose: ${guessedNum}, the machine chose: ${Math.round(Math.random()*maxNum)}`;  //round써야 0부터 maxNum까지 나오는듯?
+    result.innerHTML = `You chose: ${guessedNum}, the machine chose: ${Math.round(Math.random()*maxNum)}`;
+    /* 0부터 maxNum 사이 float 랜덤으로 얻음.
+    정수로 바꾸려면 floor(내림), round(반올림), ceil(올림) 써야하는데
+    floor 쓸 경우 maxNum이 안나오고, ceil 쓸 경우 0 나올 가능성 넘 적어져서 round 사용했음.*/
     //innerText vs innerHTML?
 
     // you won - you lost는 조건문 사용
